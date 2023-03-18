ef2b4ac296a957ae0a69de784ff9a3c3717dd147
FatmaZahra26 <zakhamafatamzahra@gmail.com>
2023-03-18 15:18:53 +0100

    first_commit

--- components/table.py
+++ components/table.py
@@ -3,30 +3,51 @@ import hvplot.pandas
 import pandas as pd
 import param
 import panel.widgets as pnw
+import seaborn as sns
 
 # Load Data
 df = pd.read_csv('StudentsPerformance.csv')
 df.rename(columns={"race/ethnicity": "race", "parental level of education": "parental_edu", "test preparation course": "test_prep",
                     "math score": "math_score", "reading score": "reading_score", "writing score": "writing_score"}, inplace=True)
 
+df["moyenne"]=((df.math_score+df.reading_score+df.writing_score)/3).round(2)
+
+
+
 # create a self-contained dashboard class
 class InteractiveDashboard(param.Parameterized):
+    #parametres 
+    gender = param.Selector(label='gender', objects=list(sorted(df.gender.unique())))
+    lunch=param.Selector(label="lunch",objects=list(sorted(df.lunch.unique())))
+    race = param.Selector(label='Race', objects=['All']+list(sorted(df.race.unique())))
+    score = param.Selector(label='Score', objects=list(set(df.columns)-set(df.select_dtypes("object").columns)))
+    parental_edu=param.Selector(label="Parental_education",objects=['All']+list(sorted(df.parental_edu.unique())))
+    test_prep=param.Selector(label="Test_prep",objects=['All']+list(sorted(df.test_prep.unique())))    
     
-    race = param.Selector(label='Race', objects=['group A','group B','group C','group D'])
-    gender = param.Selector(label='Gender', objects=['male', 'female'])
-    yaxis = param.Selector(label='Y axis', objects=['math_score', 'reading_score', 'writing_score'])
-    
-    @param.depends('gender', 'race', 'yaxis')
+
+    #gender_parent=param.Selector(label="gender_parent",objects=["parental_edu","gender"],default='gender')
+
+    #tableau
+    @pn.depends('race', 'gender', 'score')
     def plot_table(self):
-        df_widget = pn.widgets.Tabulator(df, page_size=10, layout='fit_data_table', header_align='center')
+        cm = sns.light_palette("#1C4E80", as_cmap=True)
+        df_widget = pn.widgets.Tabulator(df, header_align='center', layout='fit_data', page_size=5)
+        df_widget.style.background_gradient(subset=['moyenne'], cmap=cm, vmin=0, vmax=100)
+        df_widget.style.format({'moyenne': '{:.2f}'})
         return df_widget
     
-    def plot1(self):
-        df_filtered = df[(df.gender.isin(['male', 'female'])) & (df.race.isin(['group A','group B','group C','group D']))]
-        table = df_filtered.groupby(['gender', 'race'])[self.yaxis].mean().to_frame().reset_index()
-        plot = table.hvplot(x='gender', y=self.yaxis, by='race', kind='bar')
-        return plot
-    
+    def plot_scatter(self):
+        df.hvplot(by=[''], kind='scatter', title='Penguins Scatter', x='bill_length_mm', y=['bill_depth_mm'])
+
+    #@param.depends("gender_parent")
+    #def plot_barplot_stuck(self):
+    #    counts = df.groupby(['race', self.gender_parent]).size().reset_index(name='Count')
+    #    return counts.hvplot.bar('race', 'Count', by=self.gender_parent, stacked=True, rot=90, hover_cols=['Filter'])
+
+
+
+   
+"""    
     def plot2(self):
         df_filtered = df[(df.gender.isin(['male', 'female'])) & (df.race.isin(['group A','group B','group C','group D']))]
         plot = df_filtered.hvplot.density(y=['math_score', 'reading_score', 'writing_score'],groupby="gender", legend='top_left')
@@ -36,19 +57,26 @@ class InteractiveDashboard(param.Parameterized):
         df_filtered = df[(df.gender == self.gender)]
         plot = df_filtered.hvplot.scatter(x='math_score', y='reading_score', by='gender', legend='top_right')
         return plot
-    
+    """
 dashboard = InteractiveDashboard()
 
 # Layout using Template
 
 template = pn.template.FastListTemplate(
     title='# My Beautiful Dashboard', 
-    sidebar=[pn.Param(dashboard.param, widgets={'gender': pn.widgets.Select,'race': pn.widgets.RadioButtonGroup,'yaxis': pn.widgets.RadioButtonGroup})],
-    main=[pn.Row(pn.Column(dashboard.plot_table, dashboard.plot1)),
-          pn.Row(dashboard.plot2),
-          pn.Row(dashboard.plot3)],
-    accent_base_color="#88d8b0",
-    header_background="#88d8b0",
+    sidebar=[pn.Param(dashboard.param,widgets={
+        'gender': pn.widgets.CheckButtonGroup(name='gender',Width=0.2,button_type='success',options=list(sorted(df.gender.unique()))),
+        'lunch':pn.widgets.CheckButtonGroup(name='lunch',Width=0.2,button_type='success',options=list(sorted(df.lunch.unique()))),
+        'race': pn.widgets.Select,
+        'score': pn.widgets.Select,
+        'test_prep': pn.widgets.Select,
+        'parental_edu': pn.widgets.Select,
+        })],
+    main=[pn.Row(dashboard.plot_table)],
+          #pn.Row(dashboard.plot_barplot_stuck)],
+    accent_base_color="#1C4E80",
+    header_background="#1C4E80",
+    background_color="#EDF0F3",
 )
 
 template.servable() 
