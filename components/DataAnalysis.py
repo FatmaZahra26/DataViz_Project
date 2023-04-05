eb06193f6aaf912e7f839697f95edc657ffbe79d
FatmaZahra26 <zakhamafatamzahra@gmail.com>
2023-04-06 00:32:03 +0100

    update

--- components/DataAnalysis.py
+++ components/DataAnalysis.py
@@ -1,15 +1,23 @@
 import panel as pn
 import hvplot.pandas
 import pandas as pd
+import plotly.express as px
+
 import param
 import panel.widgets as pnw
 import seaborn as sns
 from holoviews import opts
 import matplotlib.pyplot as plt
+import holoviews as hv
 import requests
 from io import BytesIO
 from PIL import Image
-
+from bokeh.io import output_notebook, show
+from bokeh.models import HoverTool
+from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Category10
+import plotly.graph_objects as go
 
 # URL of the image to download
 male = "https://cdn-icons-png.flaticon.com/512/4537/4537047.png"
@@ -30,12 +38,12 @@ df["Total_marks"]=((df.math_score+df.reading_score+df.writing_score)/3).round(2)
 # create a self-contained dashboard class
 class InteractiveDashboard(param.Parameterized):
     #parametres 
-    gender = param.Selector(label='gender', objects=list(sorted(df.gender.unique())))
-    lunch=param.Selector(label="lunch",objects=list(sorted(df.lunch.unique())))
+    gender = param.Selector(label='gender', objects=['All'] + list(sorted(df.gender.unique())))
+    lunch=param.Selector(label="lunch",objects=['All'] + list(sorted(df.lunch.unique())))
     race = param.Selector(label='Race', objects=['All']+list(sorted(df.race.unique())))
     score = param.Selector(label='Score', objects=list(set(df.columns)-set(df.select_dtypes("object").columns)))
     parental_edu=param.Selector(label="Parental_education",objects=['All']+list(sorted(df.parental_edu.unique())))
-    test_prep=param.Selector(label="Test_prep",objects=['All']+list(sorted(df.test_prep.unique())))    
+    test_prep=param.Selector(label="Test_prep",objects=['All'] + list(sorted(df.test_prep.unique())))    
 
     gender_parent=param.Selector(label="gender_parent",objects=["parental_edu","gender"],default='gender')
     
@@ -51,50 +59,28 @@ class InteractiveDashboard(param.Parameterized):
 
 
     #tableau
-    @pn.depends('race', 'gender', 'score','lunch','parental_edu','test_prep')
+    @param.depends('race', 'gender','lunch','parental_edu','test_prep')
     def plot_table(self):
         cm = sns.light_palette("#1C4E80", as_cmap=True)
-        df_widget = pn.widgets.Tabulator(df, header_align='center', layout='fit_data', page_size=5)
+        
+        sel = [self.gender, self.lunch, self.race, self.parental_edu, self.test_prep]
+        col = ["gender", "lunch", "race", "parental_edu", "test_prep"]
+        df_ = df.copy()
+        for i, j in zip(sel, col):
+            if i != "All":
+                df_ = df_[df_[j] == str(i)]
+        df_=df_.iloc[:7,:]
+
+        df_widget = pn.widgets.Tabulator(df_,
+                                        header_align='center',widths={'index': '5%', 'gender': '10%', 'race': '10%', 'parental_edu': '15%', 'lunch': '10%','test_prep':'10%','math_score':'10%','reading_score':'10%','writing_score':'10%','Total_marks':'10%'},
+                                        sizing_mode='stretch_width',page_size=7)
+
         df_widget.style.background_gradient(subset=['Total_marks'], cmap=cm, vmin=0, vmax=100)
         df_widget.style.format({'Total_marks': '{:.2f}'})
         return df_widget
     
-    def plot_scatter(self):
-        df_scatter=df.hvplot.scatter(by="gender", title='Scatter', x='Total_marks', y=self.score ,width=500, height=400)
-        return df_scatter
-    
-    @pn.depends('race', 'gender', 'score','lunch','parental_edu','test_prep')
 
-    def plot_box(self):
-        df_box = df.hvplot.box(y=['math_score', 'reading_score', 'writing_score','Total_marks'], 
-                legend=False, value_label='Score Box Plot', invert=True,width=600, height=300,
-                )
-
-        return df_box 
-    
-    @pn.depends('score')
-    def plot_swarm(self):
-        sns.set_style('whitegrid')
-        f, ax = plt.subplots(1,1,figsize= (8,6))
-        df_swarm = sns.swarmplot(x =self.score, y='gender', data = df , palette = 'Set1')
-        plt.title(f'{self.score.capitalize()} Score Distribution by Gender')
-        plt.xlabel(f'{self.score.capitalize()} Score')
-        plt.ylabel('Gender')
-        plt.tight_layout()
-        return f
-    
-    def heatmap(self):
-        corr_matrix = df[['math_score', 'reading_score', 'writing_score']].corr()
-        fig, ax = plt.subplots(figsize=(6, 6))
-        sns.heatmap(corr_matrix, annot=True, fmt=".2f", linewidths=.5, cmap="coolwarm", ax=ax)
-        ax.set_title("Correlation Matrix")
-        ax.set_xticklabels(corr_matrix.columns, rotation=45)
-        ax.set_yticklabels(corr_matrix.columns, rotation=0)
-        plt.tight_layout()
-
-        return pn.panel(fig)
-
-    @pn.depends("gender_parent")
+    @param.depends("gender_parent")
     def plot_barplot_stuck(self):
         counts = df.groupby(['race', self.gender_parent]).size().reset_index(name='Count')
         return counts.hvplot.bar('race', 'Count', by=self.gender_parent, stacked=True, rot=90, hover_cols=['Filter'],width=600, height=400)
@@ -106,36 +92,84 @@ class InteractiveDashboard(param.Parameterized):
         plot = df_filtered.hvplot.density(y=['math_score', 'reading_score', 'writing_score'],groupby="gender", legend='top_left')
         return plot
 
-  
+    @param.depends('gender', 'lunch', 'race', 'score', 'parental_edu', 'test_prep')
+    def plot_pie(self):
+        df_copy =df.copy()
+        if (self.lunch!='All'):
+             df_copy  =  df_copy [ df_copy ['lunch']==self.lunch]
+        lunch_counts =  df_copy ['lunch'].value_counts()
+        fig = px.pie(lunch_counts, values='lunch', names=lunch_counts.index)
+        fig.update_traces(textposition='inside', textinfo='percent+label')
+        fig.update_layout(title='Lunch Distribution')
+        return fig
+    
+    @param.depends('race','lunch','parental_edu','test_prep')
+    def plot_box(self):
+        sel = [self.race,self.lunch, self.parental_edu, self.test_prep]
+        col = ["race","lunch", "parental_edu", "test_prep"]
+        df_=df.copy()
+        for i, j in zip(sel, col):
+            if i != "All":
+                df_ = df_[df_[j] == str(i)]
+
+
+        fig = go.Figure()
+        fig.add_trace(go.Box(
+            x=df_['math_score'],
+            y=df_['gender'],
+            name='math score',
+            marker_color='#3D9970'
+        ))
+        fig.add_trace(go.Box(
+            x=df_['writing_score'],
+            y=df_['gender'],
+            name='writing score',
+            marker_color='#FF4136'
+        ))
+        fig.add_trace(go.Box(
+            x=df_['reading_score'],
+            y=df_['gender'],
+            name='reading score',
+            marker_color='#FF851B'
+        ))
+
+        fig.update_layout(
+            xaxis=dict(title='Score', zeroline=False),
+            boxmode='group'
+        )
+
+        fig.update_traces(orientation='h') # horizontal box plots
+
+        return fig 
+   
+
 dashboard = InteractiveDashboard()
 
 # Layout using Template
 
 template = pn.template.FastListTemplate(
+
     title='Data Analysis', 
-    sidebar=[pn.Param(dashboard.param, width=200, widgets={
-        'gender': pn.widgets.CheckButtonGroup(name='gender', Width=0.1, button_type='success', options=list(sorted(df.gender.unique()))),
-        'lunch':pn.widgets.CheckButtonGroup(name='lunch', Width=0.1, button_type='success', options=list(sorted(df.lunch.unique()))),
+    sidebar=[
+    pn.Param(dashboard.param, width=200, widgets={
+        'gender': pn.widgets.Select,
+        'lunch':pn.widgets.Select ,
         'race': pn.widgets.Select,
         'score': pn.widgets.Select,
         'test_prep': pn.widgets.Select,
         'parental_edu': pn.widgets.Select,
     })],
+
     sidebar_width=200,
+
     main=[
     pn.Row( pn.pane.PNG(male, width=60), s2, dashboard.ind_gen1, s1, pn.pane.PNG(female, width=60), dashboard.ind_gen2, align="center"),
     pn.Row(dashboard.ind_moy1, s1, dashboard.ind_moy2, s1, dashboard.ind_moy3, align="center") ,
     pn.Row(dashboard.plot_table, sizing_mode='stretch_width'),
-    pn.Row(pn.Column(pn.pane.Markdown('## Barplot '),dashboard.plot_barplot_stuck), 
-            pn.Column(pn.pane.Markdown('## Scatter Plot'),dashboard.plot_scatter)),
-
-    pn.Row(
-    pn.Column(pn.pane.Markdown('## Box Plot'),dashboard.plot_box),
-    pn.Column(pn.pane.Markdown('## Heatmap'),dashboard.heatmap())
-    ),
-
-    pn.Row(dashboard.plot_swarm()),
+    pn.Row(pn.Column(pn.pane.Markdown('## Barplot '),dashboard.plot_barplot_stuck)),
     pn.Row(dashboard.score_distribution()),
+    pn.Row(dashboard.plot_box()),
+    pn.Row(dashboard.plot_pie()),
     ],
     accent_base_color="#1C4E80",
     header_background="#1C4E80",
--- components/ML.py
+++ components/ML.py
@@ -12,14 +12,20 @@ from PIL import Image
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
-
+from sklearn.metrics import mean_squared_error
+import statsmodels.api as sm
+import plotly.express as px
+import plotly.subplots as sp
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
 # URL of the image to download
 male = "https://cdn-icons-png.flaticon.com/512/4537/4537047.png"
 female="https://cdn-icons-png.flaticon.com/512/4537/4537148.png"
 
-s1=pn.Spacer(width=10)
-s2=pn.Spacer(width=0)
+s1=pn.Spacer(width=150)
+s2=pn.Spacer(width=50)
+s3=pn.Spacer(width=150)
 
 
 # Load Data
@@ -33,8 +39,13 @@ df["Total_marks"]=((df.math_score+df.reading_score+df.writing_score)/3).round(2)
 # create a self-contained dashboard class
 class InteractiveDashboard(param.Parameterized):
     #parametres 
-    score = param.Selector(label='Score', objects=list(set(df.columns)-set(df.select_dtypes("object").columns)))    
-    
+    x_select1 = param.Selector(label='x_select 1 for scatter plot and regression model', objects=['math_score','reading_score','writing_score'])    
+    x_select2 = param.Selector(label='x_select 2 for scatter plot and regression model', objects=['math_score','reading_score','writing_score'])    
+
+    y_select = param.Selector(label='y_select for scatter plot and regression model', objects=['math_score','reading_score','writing_score'])    
+    n_clusters = pn.widgets.IntSlider(name='n_clusters', start=1, end=5, value=3)
+
+
     values_moyenne = [int(df['Total_marks'].between(70, 100).sum()*100/len(df)), int(df['Total_marks'].between(50, 70).sum()*100/len(df)), int(df['Total_marks'].between(0, 50).sum()*100/len(df))]
     ind_moy1=pn.indicators.Number(name='EXCELLENCE', value=values_moyenne[0], format='{value}%',colors=[(88, 'green')],font_size='20pt',title_size='12pt')
     ind_moy2=pn.indicators.Number(name='MEDIOCRITY', value=values_moyenne[1], format='{value}%',colors=[(66, 'gold')],font_size='20pt',title_size='12pt')
@@ -44,46 +55,89 @@ class InteractiveDashboard(param.Parameterized):
     ind_gen1=pn.indicators.Number(name='Male', value=values_gender[0], format='{value}%',default_color="blue",font_size='20pt',title_size='12pt')
     ind_gen2=pn.indicators.Number(name='Female', value=values_gender[1], format='{value}%',default_color="red",font_size='20pt',title_size='12pt')
 
-    #tableau
-    @pn.depends('score')
+    
+    @pn.depends('y_select')
+    def plot_qqplot(self):
+        fig, ax = plt.subplots()
+        qqplot = sm.qqplot(df[self.y_select], line='45', ax=ax)
+        ax.set_title(f'Q-Q Plot of {self.y_select} with Qauntile Theoriques')
+        ax.set_xlabel("Qauntile Theoriques")
+        ax.set_ylabel(self.y_select)
+        return fig
+
+    @pn.depends('x_select1','y_select','x_select2')
     
     def plot_scatter(self):
-        df_scatter=df.hvplot.scatter(by="gender", title='Scatter', x='Total_marks', y=self.score ,width=700, height=500)
+        df_scatter=df.hvplot.scatter(by="gender", title='Scatter', x=self.x_select1, y=self.y_select ,width=600, height=500)
         return df_scatter
     
+    def heatmap(self):
+        corr_matrix = df[['math_score', 'reading_score', 'writing_score']].corr()
+        fig, ax = plt.subplots(figsize=(7, 7) ,facecolor='none')
+        sns.heatmap(corr_matrix, annot=True, fmt=".2f", linewidths=.5, cmap="coolwarm", ax=ax)
+        ax.set_title("Correlation Matrix")
+        ax.set_xticklabels(corr_matrix.columns, rotation=45)
+        ax.set_yticklabels(corr_matrix.columns, rotation=0)
+        plt.tight_layout()
+
+        return pn.panel(fig)
+    
+
+    @param.depends('y_select','x_select1','x_select2')
+    #selection y ==> x1 , x2 fixé
     def regression_model(self):
-        y = df[self.score]
-        X = df.drop([self.score ,'gender', 'race', 'parental_edu', 'test_prep','lunch'], axis=1)
-        
+        lr_model = LinearRegression()
+        y = df[self.y_select]
+        X = df[[self.x_select1,self.x_select2]]
+        scaler = StandardScaler()
+        #X= scaler.fit_transform(X).reshape(1,-1)
+        #y= scaler.fit_transform(y).reshape(-1,1)
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        lr_model = LinearRegression()
         lr_model.fit(X_train, y_train)
+        y_pred = lr_model.predict(X_test)
 
-        r2 = lr_model.score(X_test, y_test) * 100
-        score = pn.indicators.Number(name='R2 score', value=r2, format='{value:.3f}',font_size='20pt',title_size='12pt')
-        return score
+        r2 = r2_score(y_test, y_pred) * 100
+        rmse =mean_squared_error(y_test, y_pred , squared=False) 
 
+        r2_s = pn.indicators.Number(name='R2 Score', value=r2 , format='{value:.3f}',colors=[(85, 'red'),(95,'gold'),(100,'green')],font_size='17pt',title_size='17pt')
+        rmse_s = pn.indicators.Number(name='RMSE Score', value=rmse , format='{value:.3f}',colors=[(85, 'red'),(95,'gold'),(100,'green')],font_size='17pt',title_size='17pt')
+
+        return pn.Row(s1,r2_s,s3,rmse_s)
+    
+    @pn.depends(x.param.value, y.param.value, n_clusters.param.value)
+    def get_clusters(self):
+        scaler=StandardScaler()
+        X = scaler.fit_transform(df[['math_score', 'reading_score', 'writing_score']])
+        kmeans = KMeans(X)
+        labels = kmeans.labels_.astype(str)
+        centers = X.groupby('labels').mean()
+        return (X.sort_values('labels').hvplot.scatter(X, y, c='labels', size=100, height=500) *
+                centers.hvplot.scatter(X, y, marker='x', color='black', size=400,
+                                    padding=0.1, line_width=5))
 
 
 
 dashboard = InteractiveDashboard()
-
 # Layout using Template
 
 template = pn.template.FastListTemplate(
     title='Machine Learning', 
     sidebar=[pn.Param(dashboard.param, width=200, widgets={
-        'score': pn.widgets.Select,
+        'x_select1': pn.widgets.Select,
+        'x_select2': pn.widgets.Select,
+        'y_select':pn.widgets.Select,
+        'n_clusters': pn.widgets.IntSlider,
     })],
     sidebar_width=200,
     main=[
     pn.Row( pn.pane.PNG(male, width=60), s2, dashboard.ind_gen1, s1, pn.pane.PNG(female, width=60), dashboard.ind_gen2, align="center"),
     pn.Row(dashboard.ind_moy1, s1, dashboard.ind_moy2, s1, dashboard.ind_moy3, align="center") ,
-    pn.Row(pn.pane.Markdown('## Scatter Plot'),dashboard.plot_scatter),
-    pn.Row(dashboard.regression_model()),
+    pn.Row(dashboard.plot_qqplot()),
+    pn.Row(pn.Column(pn.pane.Markdown('## Scatter Plot'),dashboard.plot_scatter),pn.Column(pn.pane.Markdown('## HeatMap'),dashboard.heatmap())),
+    pn.Row(pn.pane.Markdown('## Linear Regression Result : '),dashboard.regression_model),
     ],
-
     accent_base_color="#1C4E80",
     header_background="#1C4E80",
     background_color="#EDF0F3",
